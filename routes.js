82e210e38e04d5b17fa1f96b9cea32d0649728c0
Kenneth Kouot <kenneth@wikia-inc.com>
2014-03-05 13:07:15 -0800

    remove irrelevant comment

--- routes.js
+++ routes.js
@@ -1,8 +1,6 @@
 var path = require('path');
 
 module.exports = function (server) {
-	// Set up static assets serving, this is probably not a final implementation as we should probably setup
-	// nginx or apache to serve static assets and route the rest of the requests to node.
 	server.route({
 		method: 'GET',
 		path: '/{path*}',
